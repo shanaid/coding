c6c68970e271d940506cda2eae9efa53c6b897ba
shanaid <125241066+shanaid@users.noreply.github.com>
2025-09-08 22:22:16 +0900

    [Gold IV] Title: 문자열 폭발, Time: 320 ms, Memory: 26004 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,41 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Stack;
 
-public class Main { // BOJ면 반드시 Main
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[] text = br.readLine().toCharArray();
         char[] boom = br.readLine().toCharArray();
-        int m = boom.length;
-
-        // 문자 스택 (배열 + top)
+        int boomSize = boom.length;
         char[] stack = new char[text.length];
         int top = 0;
+        for(char i : text ){
+            stack[top++] = i;
 
-        for (char c : text) {
-            stack[top++] = c; // push
-
-            // 스택 끝이 boom과 같은지 확인
-            if (top >= m) {
-                boolean ok = true;
-                for (int i = 0; i < m; i++) {
-                    if (stack[top - m + i] != boom[i]) {
-                        ok = false; break;
+            a:if(top >= boomSize){ //boom을 버티면
+                if(boom[boomSize-1] == stack[top-1]){ // 만약 끝에가 같다하믄
+                    for(int t = 1; t<boomSize; t++){ // 전에 부터 쏵 비교해
+                        if(stack[top-1-t] != boom[boomSize-1-t]){ //비교해서 하나라도 틀리면
+                            break a; //break
+                        }
                     }
-                }
-                if (ok) {
-                    top -= m; // boom 길이만큼 "폭발" (pop m번 대신 포인터만 이동)
+                    // 여기있다는건 터졌다는 거
+                    top -= boomSize;
                 }
             }
-        }
 
+
+        }
         if (top == 0) {
             System.out.print("FRULA");
         } else {
             StringBuilder sb = new StringBuilder(top);
             for (int i = 0; i < top; i++) sb.append(stack[i]);
             System.out.print(sb.toString());
+
         }
+
     }
 }
